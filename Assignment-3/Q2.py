37e70d75f2c6cb3ec1dcbae9b4d3fe0935b58f84
Pabitra Ranjan Giri <111499009+QuantumHero10@users.noreply.github.com>
2022-10-09 19:00:55 +0530

    Update Q2.py

--- Assignment-3/Q2.py
+++ Assignment-3/Q2.py
@@ -1,7 +1,7 @@
 import mylibrary.chy as chy #Importing the code for Cholesky decomposition method
 import mylibrary.gas as gas #Importing the code for Gauss-Seidel method
 def main():
-    path='C:\\Users\\manor\\Desktop\\P346 codes\\matrix2.csv' #Augmented matrix full path
+    path='Assignment-3\\matrix2.csv' #Augmented matrix full path
     chy.main(path) #Solving linear equations using Cholesky decomposition method
     gas.main(path) #Solving linear equations using Gauss-Seidel method
 
