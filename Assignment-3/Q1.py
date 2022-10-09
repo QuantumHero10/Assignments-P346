e8e0fb29c2ba435bef1d94951bb937138b765d05
Pabitra Ranjan Giri <111499009+QuantumHero10@users.noreply.github.com>
2022-10-09 19:00:24 +0530

    Update Q1.py

--- Assignment-3/Q1.py
+++ Assignment-3/Q1.py
@@ -1,7 +1,7 @@
 import mylibrary.gje as gje #Importing the code for Gauss-Jordon elimination method
 import mylibrary.lud as lud #Importing the code for LU decomposition method
 def main():
-    path='Assignment-3//matrix1.csv' #Augmented matrix full path
+    path='Assignment-3\\matrix1.csv' #Augmented matrix full path
     gje.main(path) #Solving linear equations using Gauss-Jordon elimination method
     lud.main(path) #Solving linear equations using LU decomposition method
 
