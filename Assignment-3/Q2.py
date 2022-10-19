648e0903b971a7f2554be9fa2ebb34ddbcab7b21
Pabitra Ranjan Giri <111499009+QuantumHero10@users.noreply.github.com>
2022-10-19 19:49:35 +0530

    Delete Assignment-3 directory

--- Assignment-3/Q2.py
+++
@@ -1,8 +0,0 @@
-import mylibrary.chy as chy #Importing the code for Cholesky decomposition method
-import mylibrary.gas as gas #Importing the code for Gauss-Seidel method
-def main():
-    path='Assignment-3\\matrix2.csv' #Augmented matrix full path
-    chy.main(path) #Solving linear equations using Cholesky decomposition method
-    gas.main(path) #Solving linear equations using Gauss-Seidel method
-
-main()
--- Assignment-3/Q3.py
+++
@@ -1,8 +0,0 @@
-import mylibrary.gas as gas #Importing the code for Gauss-Seidel method
-import mylibrary.lud as lud #Importing the code for LU decomposition method
-def main():
-    path='Assignment-3\\matrix3.csv' #Augmented matrix full path
-    lud.main(path) #Solving linear equations using Gauss-Seidel method
-    gas.main(path) #Solving linear equations using LU decomposition method
-
-main()
