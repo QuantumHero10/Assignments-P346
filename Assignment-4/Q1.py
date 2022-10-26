fac1222201605fdcbcec59e768449c83292c5442
Pabitra Ranjan Giri <111499009+QuantumHero10@users.noreply.github.com>
2022-10-26 21:02:51 +0530

    Add files via upload

---
+++ Assignment-4/Q1.py
@@ -0,0 +1,16 @@
+import math
+import mylibrary.nb as nb #Importing the code for Bisection method
+import mylibrary.rf as rf #Importing the code for Regula-falsi method
+
+def f(x):  # Non linear equation
+    if x<0:
+        x=1.0
+    return (math.log(0.5*x) - math.sin(2.5*x))
+
+def main(f):
+    a= 1.5 #Interval
+    b= 2.5
+    nb.main(f,a,b) #Solving linear equations using Bisection method
+    rf.main(f,a,b) #Solving linear equations using Regula-falsi method
+
+main(f)
---
+++ Assignment-4/Q3.py
@@ -0,0 +1,6 @@
+import mylibrary.lgr as lgr #Importing the code for Laguerre method
+def main():
+    A = [1,0,-5,0,4] # Matrix represntation of the polynomial equation
+    lgr.main(A) #Solving linear equations using Laguerre method
+
+main()
